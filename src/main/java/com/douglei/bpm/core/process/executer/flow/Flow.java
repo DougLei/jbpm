b5fc8d6fc736ed77a40f12bf8e5c7734138733e1
DougLei <953034206@qq.com>
2020-09-17 18:11:23 +0800

    t

--- src/main/java/com/douglei/bpm/core/process/executer/flow/Flow.java
+++ src/main/java/com/douglei/bpm/core/process/executer/flow/Flow.java
@@ -1,16 +1,17 @@
 package com.douglei.bpm.core.process.executer.flow;
 
 import com.douglei.bpm.core.process.executer.Node;
+import com.douglei.bpm.core.process.executer.task.Task;
 
 /**
  * 
  * @author DougLei
  */
 public class Flow extends Node{
-	private String source;
-	private String target;
 	private int order;
 	private String conditionExpr;
+	private Task targetTask;
+	
 	public int getOrder() {
 		return order;
 	}

e9d7e4c6b8df71751f517111a96d360a2ab2c0fd
DougLei <953034206@qq.com>
2020-12-15 17:56:45 +0800

    t

--- src/main/java/com/douglei/bpm/process/handler/task/user/UserTaskHandler.java
+++ src/main/java/com/douglei/bpm/process/handler/task/user/UserTaskHandler.java
@@ -1,13 +1,11 @@
 package com.douglei.bpm.process.handler.task.user;
 
-import com.douglei.bpm.bean.annotation.Autowired;
 import com.douglei.bpm.bean.annotation.Bean;
 import com.douglei.bpm.module.ExecutionResult;
 import com.douglei.bpm.module.runtime.task.Task;
 import com.douglei.bpm.module.runtime.task.assignee.Assigners;
 import com.douglei.bpm.process.Type;
 import com.douglei.bpm.process.handler.GeneralExecuteParameter;
-import com.douglei.bpm.process.handler.ProcessHandlers;
 import com.douglei.bpm.process.handler.TaskDispatchParameter;
 import com.douglei.bpm.process.handler.TaskHandler;
 import com.douglei.bpm.process.metadata.node.task.user.UserTaskMetadata;
@@ -18,11 +16,8 @@ import com.douglei.orm.context.SessionContext;
  * @author DougLei
  */
 @Bean(clazz=TaskHandler.class)
-public class UserTaskExecutor extends TaskHandler<UserTaskMetadata, TaskDispatchParameter, GeneralExecuteParameter>{
+public class UserTaskHandler extends TaskHandler<UserTaskMetadata, TaskDispatchParameter, GeneralExecuteParameter>{
 
-	@Autowired
-	private ProcessHandlers handlers;
-	
 	@Override
 	public ExecutionResult startup(UserTaskMetadata userTask, TaskDispatchParameter parameter) {
 		Task task = new Task(parameter.getProcdefId(), parameter.getProcinstId(), userTask);

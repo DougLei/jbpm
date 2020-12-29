19008d4ff3b5e7c2f68f67168067dc45031d756b
DougLei <953034206@qq.com>
2020-12-29 16:35:02 +0800

    t

--- src/main/java/com/douglei/bpm/process/metadata/TaskMetadata.java
+++ src/main/java/com/douglei/bpm/process/metadata/TaskMetadata.java
@@ -31,25 +31,4 @@ public abstract class TaskMetadata extends ProcessNodeMetadata{
 	public boolean requiredUserHandle() {
 		return false;
 	}
-	/**
-	 * 任务是否需要其关联的输入流集合, 默认值为false
-	 * @return
-	 */
-	public boolean requiredInputFlows() {
-		return false;
-	}
-	/**
-	 * 任务是否需要其关联的输出流集合, 默认值为false
-	 * @return
-	 */
-	public boolean requiredOutputFlows() {
-		return false;
-	}
-	/**
-	 * 任务是否需要默认的输出流, 默认值为false
-	 * @return
-	 */
-	public boolean requiredDefaultOutputFlow() {
-		return false;
-	}
 }
--- src/main/java/com/douglei/bpm/process/metadata/TaskMetadataEntity.java
+++ src/main/java/com/douglei/bpm/process/metadata/TaskMetadataEntity.java
@@ -1,6 +1,7 @@
 package com.douglei.bpm.process.metadata;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import com.douglei.bpm.process.metadata.flow.FlowMetadata;
@@ -10,26 +11,26 @@ import com.douglei.bpm.process.metadata.flow.FlowMetadata;
  */
 public class TaskMetadataEntity<M extends TaskMetadata> {
 	private M taskMetadata;
-	private List<FlowMetadata> inputFlows;
-	private List<FlowMetadata> outputFlows;
-	private FlowMetadata defaultFlow;
+	private FlowMetadataList outputFlows;
+	private FlowMetadata defaultOutputFlow;
+	private FlowMetadataList inputFlows;
 
 	TaskMetadataEntity(M taskMetadata, List<FlowMetadata> flows) {
 		this.taskMetadata = taskMetadata;
 		
 		for (FlowMetadata flow : flows) {
-			if(taskMetadata.requiredInputFlows() && flow.getTarget().equals(taskMetadata.getId())) {
-				if(this.inputFlows == null)
-					this.inputFlows = new ArrayList<FlowMetadata>(4);
-				this.inputFlows.add(flow);
-			}
 			if(taskMetadata.requiredOutputFlows() && flow.getSource().equals(taskMetadata.getId())) {
 				if(this.outputFlows == null)
-					this.outputFlows = new ArrayList<FlowMetadata>(4);
-				this.outputFlows.add(flow);
+					this.outputFlows = new FlowMetadataList();
+				this.outputFlows.addFlow(flow);
 			}
-			if(taskMetadata.requiredDefaultOutputFlow() && taskMetadata.getDefaultOutputFlowId() != null && this.defaultFlow==null && flow.getId().equals(taskMetadata.getDefaultOutputFlowId())) {
-				this.defaultFlow = flow;
+			if(taskMetadata.getDefaultOutputFlowId() != null && flow.getId().equals(taskMetadata.getDefaultOutputFlowId())) {
+				this.defaultOutputFlow = flow;
+			}
+			if(taskMetadata.requiredInputFlows() && flow.getTarget().equals(taskMetadata.getId())) {
+				if(this.inputFlows == null)
+					this.inputFlows = new FlowMetadataList();
+				this.inputFlows.addFlow(flow);
 			}
 		}
 	}
@@ -37,13 +38,49 @@ public class TaskMetadataEntity<M extends TaskMetadata> {
 	public M getTaskMetadata() {
 		return taskMetadata;
 	}
-	public List<FlowMetadata> getInputFlows() {
-		return inputFlows;
-	}
 	public List<FlowMetadata> getOutputFlows() {
-		return outputFlows;
+		if(outputFlows == null)
+			return null;
+		return outputFlows.getFlows();
 	}
-	public FlowMetadata getDefaultFlow() {
-		return defaultFlow;
+	public FlowMetadata getDefaultOutputFlow() {
+		return defaultOutputFlow;
 	}
+	public List<FlowMetadata> getInputFlows() {
+		if(inputFlows == null)
+			return null;
+		return inputFlows.getFlows();
+	}
+	
+	// 流集合实例
+	private class FlowMetadataList {
+		private boolean sorted;
+		private List<FlowMetadata> flows;
+		
+		public void addFlow(FlowMetadata flow) {
+			if(flows == null)
+				flows = new ArrayList<FlowMetadata>(4);
+			flows.add(flow);
+		}
+		public List<FlowMetadata> getFlows() {
+			if(!sorted && flows.size() > 1) {
+				flows.sort(flowSortComparator);
+				sorted = true;
+			}
+			return flows;
+		}
+	}
+	
+	// flow的排序比较器
+	private static final Comparator<FlowMetadata> flowSortComparator = new Comparator<FlowMetadata>() {
+		@Override
+		public int compare(FlowMetadata f1, FlowMetadata f2) {
+			if(f1.getOrder() == f2.getOrder()) {
+				return 0;
+			}else if(f1.getOrder() < f2.getOrder()) {
+				return -1;
+			}
+			return 1;
+		}
+	};
 }

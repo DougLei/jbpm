84323c8d66ac53b692f53295b34beed773f96a0f
DougLei <953034206@qq.com>
2021-01-04 09:14:08 +0800

    t

---
+++ src/main/java/com/douglei/bpm/process/handler/TaskEntity.java
@@ -0,0 +1,27 @@
+package com.douglei.bpm.process.handler;
+
+import com.douglei.bpm.module.runtime.task.Task;
+
+/**
+ * 
+ * @author DougLei
+ */
+public class TaskEntity {
+	private Task task;
+	private boolean createBranch; // 是否创建分支
+	
+	public TaskEntity(Task task) {
+		this.task = task;
+	}
+	public TaskEntity(Task task, boolean createBranch) {
+		this.task = task;
+		this.createBranch = createBranch;
+	}
+	
+	public Task getTask() {
+		return task;
+	}
+	public boolean isCreateBranch() {
+		return createBranch;
+	}
+}

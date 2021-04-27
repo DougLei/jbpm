e8dfc56bf67ac73f43459f001e3d9f1bf46d3958
DougLei <953034206@qq.com>
2021-04-27 17:53:10 +0800

    t

--- src/main/java/com/douglei/bpm/process/mapping/parser/GeneralParser.java
+++ src/main/java/com/douglei/bpm/process/mapping/parser/GeneralParser.java
@@ -1,9 +1,13 @@
 package com.douglei.bpm.process.mapping.parser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.dom4j.Element;
 
+import com.douglei.bpm.bean.annotation.Autowired;
+import com.douglei.bpm.process.api.APIContainer;
+import com.douglei.bpm.process.api.listener.Listener;
 import com.douglei.bpm.process.mapping.metadata.ProcessNodeMetadata;
 
 /**
@@ -11,6 +15,9 @@ import com.douglei.bpm.process.mapping.metadata.ProcessNodeMetadata;
  * @author DougLei
  */
 public abstract class GeneralParser {
+	
+	@Autowired
+	private APIContainer apiContainer;
 	
 	/**
 	 * 解析条件表达式
@@ -41,6 +48,13 @@ public abstract class GeneralParser {
 		if(elements.isEmpty())
 			return;
 		
-		// TODO 添加配置的监听器
+		List<String> listeners = new ArrayList<String>(elements.size());
+		elements.forEach(elem -> {
+			Listener listener = apiContainer.getListener(elem.attributeValue("value"));
+			if(listener == null)
+				throw new ProcessParseException(String.format("<%s id=%s name=%s><listeners><listener>的value属性值[%s]不合法", metadata.getType().getName(), metadata.getId(), metadata.getName(), elem.attributeValue("value")));
+			listeners.add(listener.getName());
+		});
+		metadata.setListeners(listeners);
 	}
 }
